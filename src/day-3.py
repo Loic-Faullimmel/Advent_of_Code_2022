6aa63d84cc29760551577f373cb3d2121dea4d2d
Loïc Faullimmel <loic.faullimmel@viacesi.fr>
2022-12-21 14:16:41 +0100

    Create day-3.py solution
    Include exercice asset.

---
+++ src/day-3.py
@@ -0,0 +1,78 @@
+inputRucksacks = ["VdzVHmNpdVmBBCpmQLTNfTtMhMJnhFhTTf", "FgqsZbqDDFqRrhhJnsnLMTfhJG", "bRRRPrRRwSwbDqgjvDZbRbQzpzmQVWCzzBdvQBFCzlWV", "GcDdRdvhRssRhGDdShCRtqWjlQzqWgqzNfNjfQWWjt", "mwwnnPFwmVrPmJmzfNzqCjQCbgVlgC", "nPnHHLrHwmJTrCTJpThBscBSdSLGZvZBvRhZ", "RVQQcVlcSRclfZCCCnMJJTSTnC", "NdHwjdwjbBBZrrZrbJDZJJ", "wmhjGGBGwwmjtjtdPlfRcpVQlhRppVJF", "pplbNBPPrppllrFNbpvppSTcwqcWFhTTShhJDTchqd", "RGzRfLjjmZmfmwLftTWhStStJWTdWmDm", "nfsMjQssnpPvNnrPrw", "SjjBgllzlQjBZvlBBgcFbgJHsMhJqbMHPggJbM", "hRLRVDdRRWnJqnnHTqMCnH", "GRfLddRRpVhNVrWSjwQQzSzcGSBQSc", "qMwNqqBdQdnTVBBVVhMVnVFzTHPggTPjGRDzvPTjjmvPDj", "sbSrWJpStrtPtRPttzmmDD", "pfbJJcbsrcLpWLllsnFmcqcwQncnQQqVNQ", "RBTWCMwCwdZThPZcZZ", "pVmVpHLFFFHHVgVmvNmHSQNvddlPPzZJMPcdhclhjczLdZMP", "vnnNnFStGMRDwWnn", "fWDdJTpDJzdBBBdmDSbSRHRwPqbPbHgSbz", "slQtQvNsMVvrrgPRgRglnhwWPH", "sGMMQFrsjvNMfWmdpfFDFZBf", "vnMRMWCMJwWWwwWPjmSdVmLdzvVbLrhL", "HsNfDHQlZpNqfQzbLbrqhjLmVdjd", "dfDZQsNpstHHHptZDDtZWgngtgBMPMMRwCPtBBGW", "HwQwwbwFNWHwHBVFQFLQzRznZnSzcjjjpPbcPpSP", "vTfTJsCmsftJZmTSSdPvzdjRSvPdjd", "TrGtTJfmGDfDhrhJJJsqrZhDBFLHHLLQWFwwlWBVBBVwgLFD", "FFTJRLccQgmTbSsbGm", "PBPPqCvCwqwhQQVhQngmVmSgglmGnHbnmb", "zqthvtQPBfCCzPwQPtwQzPwNLfNRFNLdLRLFRFFNLFdFdW", "nszjQnsPwjznzCCrhJqvjqhmBv", "tFWdHGWFGtctlNNpZBBhmqTrrbWqvTBT", "dlFtcpHDVVVHFdNGHGpGfQgsPDzSMsQwwPwgLLBQ", "TzQqTJGvnnSzqrWTnvfbbcflQcVltfcCMPVM", "jFjNZFFJLpFwmBwblcpptcVtfbbVlR", "jmmJdBBLNdGDWDDrdzqn", "pzddqQmGgbqgGpbJmmdnLZDCRZnZvFlLRZLSlLRT", "rVwchcBBMwVBHhHTZCTSGSCRTZlTDr", "HtccPfjfBhMtVBGHWpNqJdJdpjNJppWz", "WThTWWhtPbZRvvWbbvRTSRMjVRLLgFssgLpVsfSF", "JdwrlJcCwfzdqwwjsjzpLMgVsMFgML", "lQrwHNGJHClvTmfhBmPQmt", "lbRLhcLRpLJzgdGddF", "qvhwqDDCVtBDVhfMVGFnzGGzTBnGzGGgFg", "VjCwCWCMtjVDtChvQhtffcSmHpNWrrcHZHHZpplWbp", "DJVDVdvpmZdPgrCbgbgCJC", "lzczcWwwznGhBgPSvTlCrNgqNC", "wGzzQhzGGsBBGRBcQwGwnwjmmRHpRfmmMpppMjjHDvLL", "HJjJQWjFmmWtFmJTMchghhDwNMhVMWML", "SznPSRfRSSPdrrPSShbDVhbLPwcwGGwVNh", "ddRfzdRrCrRsZDSnFjspvFvqFqFqTvJt", "lflfjQfjvljfbfMLTTDCmHNLNVbL", "HSJnRrrJZJssnGRrnsrcqqRnDCLBMhVCTLVLhVNVJBBBhhBm", "SsrGGqqnSsWSnnqWHSrPfzftvFdvWlwfQgQwWvzz", "nQlsGnFGwwqNJWmJJjpplt", "HMTLPTRdvsTCCThDCZdLdLDNNpJBWJbjJMpBmbtNptBWmm", "DLCzPzTzZDdLdGSGfSGrsnQGzr", "LNPPLHNPHQNQSBFDWDPgggFv", "hszfWCWJhrBMsSSBgvFD", "GGZjfmJTjmZfrJrZrZJRGwNQnlLNHWjLVjlwdVNHpV", "BdNVdTcGVclmTwrTnwPwrHCr", "zttBWzfLsCggHPwDrf", "szsWSMbWzzbqBbzJjtjsvMzzvdmdVpGllpcRNZZhmRpZcGGc", "CjdbMmmmZFnzzgHlttGBVqtBGtsldG", "LvPPWNcFSSRslWhBsllT", "ppccvLPpcSNwLLwrDNNpLvwJHCMDmbCJbFzgmZZmFgbgnM", "TTNRwZqhcTTjsNTTsmrJlvrmmmqqHSrlJH", "fLQCCdtcfCDDVbVVQdFbQbdJHMHrJHrHnMllHdMHPrMdln", "WfQLQWWDbwRTWcRssN", "HQGQWHPDHNjMNQGNWNTWCvZllzqFZqzvvzhCtvFj", "DfgwdgfcFpchztvt", "sRggdwwVdgmnSTnnDBPBNWLn", "WbCZCfTVTTJjSwGdWNDGGw", "MMRqggMsqhlmlhrssHgRnRmRvdzdczvdNGNLzScGDrNzrLNc", "lRqsnRhmqqQnQpgQMlgDqRfBTJVFbJZQtBCbZQJVZFFb", "JnhQcCnmLDsmgmgr", "bbMZppRFGGRPfBMMRGMZssTTrLlLfsLlVLdsLsdn", "GZGSpPGMZtGGPFFRGBCwhvwjjcnJctvQcvHq", "vvrPrHZMGJNRMnqn", "BVChWWcDVWsBwCWwGrJNhRLJJnJtLqnq", "cjDfcfpWWsfWccBsHgPgrPTdpZbbgggv", "GshtVtVtjSCVtVvVGtlVvFZLMvLRZmHmZwbLwZdLdZmR", "JWzNDQzjcgJgQBJgzgMwLLHZZcdPwRLwRdHZ", "QWTppBWfDrrNBTTfffhFCpVSjnhCGsFtsqSl", "nmbCnzHHNzCjCJHJNSCWHLBLrvBrrSGRBDhrDRLrGL", "TVtPllwcgdmTRhLQTQhT", "fdFtccFcpPmggfdfNzHzCMsbCnWnJs", "fMgddvjgRRvjvjVJVdTlZGGtGnrlnqTccNjl", "HHSFSWSmmpbBpZlGncrNGbNtrn", "WDWBDDBDBDCwPBWBDWNQDgzvVvLRvsVLRwvwdJVLwL", "ZSmmvcpsmcJmJvqgBZgZqqtCtZjl", "WhDwhFSDgtBFjnFg", "rTrSTLWTTHNMNwNrMVddwNNhsmJGQcRsRcJGsJzQJsrzPsPm", "GBtLmPsCQqsGqgghZHDzzgLbFz", "zjjVTzTlRjRJfznrvrfpnNhFSghbbNFgHrbHZbDHbH", "vpfcTJVpcVlfcQPMPCGzCBsd", "HMhZNffcPZfNMrzjjFdGcJDjvJ", "VSBVVLlSQQmTVSWpSQzDrHzTTvDvFjFdGGzT", "mQSplVHWbHLSgWQnShNwsZMZPfbsNCRNCt", "MMqvDzLwZzlMqQfdGWPfgPffPglH", "ShTcJshsrRdnrdfrrfHp", "VVRtFhsCJVJVvwQqDdbDQd", "dmnNMlFNvmvljnbpMWNDFQvfQJJGvfPCfHGgQQgcJg", "bRVzLBSSTRBRBBrwTrVtRwCfcGHcsJgJgzgGsggHCzcC", "ZbtVTTrrqrSSVwhqqwBRwFWMDFNdjdZpWjdDDppjMW", "MTzqtbLtwFzJgbHgfbdWWH", "VMNBjNVjvNfhhhhfNPhP", "jmGMvlDZZnVMtzlwzqqCpwFt", "PpzGspGmpPsFLrTnTLzzBg", "QCWvfjfWjRPFZgrvqrBvTg", "wwNRCNQQVNRWjNWfQbHCCClHGDGJdGhpdhtPGhltDlJD", "dhbpGzhllzGlPvnzNcvtNVnc", "gcFMsTJDMMwrZqfjjqvvfnPtqJ", "sWRWTRFwrTgLDDFWgMsTlpSlpbSCdWWdcbmpChGd", "QccdFFFcFbcQPQPHMgpPMp", "NJlNSSMLDfJfmlSqHZRNpRqNBRPRPq", "LlMmJfvDVVTJSmVMscsCFtvwcjWjrjCj", "NVVMGWFSMRVGWSthwhTJWzcJCcJsTs", "jqRLqlfRZcmjcCzT", "rlRRrdrflpdvPbHpflfPlfDBgBMQpGVQMgpVDGMggBDV", "VwRhccRsnQStRhtGQVQVsmjgDgqJdggDjqLDgJlLzmLl", "BWFZpWHBNCBCNBzBNvWBpzHZqqlMqgNdlllDdqDgJDLlfDdd", "pFbTrrrBzbzTtSwStQnnsrVn", "DRfFbFqzbddfPFtsJnJRsnClJRsn", "cgjgQgWvSLVQgmWWgWVjVSSSBTltThLnqJssnTCZsTThntZT", "qjwpSrmWgcSrGMfdFDFdwHFd", "RWjDDWDjDNjjgDtSRRgjcjzFpnzwdFbFNdbFbpnldwFF", "vQfPfTQJbZdThTzL", "PrBQJQsfQqrrbfmPqMBfJbggjRVgWjttsHRSgRctDjSs", "NgqNWqqWWdnJdqpBNFtCmJGCDHttDGDsHsHm", "BjvzhRLTrTBQhTMQRjRRcjPGtmDCZZDZSCmmMSSZmVmSSt", "vQzRvRzQcPcvfQzRnddppFgnFfWwBFlb", "nnPvfvgrtPDHgvvGTRRRPZQGpGCLLV", "FlBsBdbllFdfWpbGMCVMZLVbZQ", "lhchNcqFsJBlBszztvwHjvzgrzmzffgH", "zZhdjTpJJpjmmpPZhvqnnZHqZcggvgMbgv", "tFpFQFSFtBGlFNwFfNMnHfbHcnvcvcfvcqrM", "GBFlNLSNVGVSSGtQSLLBBlNtphDdzpmmPmTPhRmdzdVCCDdR", "rpRCCDLpmnCdJCjn", "vMhSFvgsMGLmnmWMmm", "wVqFFvwvPPHhFhhgHPwHshpqrDDzqlfRbpftRLblrllr", "CRNDzdJCVDWzVgDjdjzRJzWRMTbHsMNZNbZMMbsfhTtMTLMB", "wSlwQcSpqPpcqcqFSqpwslsTfZtLhtlthtBHtTMZ", "SPGFGFFmpcPGDrWDmjDJVffR", "dsmdtJthJphWqHRPnRRsvvnnfR", "cDBMDDDlBZglDZTMDfzVvNRrvNPVHzRRTV", "PGMCCDClBDDbbFqmmhqQdpWGmmWp", "BJjcGhcvCnBdGHsmHSzZDzSDMHmRMQ", "qLWPLVrTwWlwwwrfrFfGDNmDQRMbQMzzmmbQLNMM", "rVWrFlGqlqwVwVGgWGphnvgBBsnvsjdBnCBnBg", "sNNsfBsmcGmgNTcHHSpnTWHnpV", "QlrhlrlMglhDQrdFblvFtMdnDWwSHDWWwnTSjLwVDSwwwT", "QtdMvltZhbFlPPZbQtQthZQdqCsJJGzBqqCBmCNCqgRCBsfP", "SZnQnnHRWRQRVjHnqlJTQPfdlqfJftqG", "pDzmbDBFbBLvvzttfdlTTl", "gsDLLpcmsSZVwlnRsV", "LHsWjwjWqCLsqCHcLsjdLqcdbpMGZPPtBhthbZBpBhMllwPG", "VFnVbbvJSfbgphSpGlhRBBSP", "JrrTgmFgzvNbrmNnmnvzgTLjCQWDLDCsTjssjqcHLc", "QmwwqTqsrdqNNqgtvnVDVcGNNtvv", "WBFBpzzjSJBJzJbfntgPzVzcvPnzDf", "HcpbHZJBFpjpcSZrZsdRQZrCwrwd", "JqmLmbtTWThBTWvWGVSrrVDsSGSG", "wwzRzNjNNbsPVPds", "jfgQRZwpQclQfffHgpRpwpfTcqtLLqCbbFFFLmbmTTBnFB", "fGpcccNNqcctqGMprvMPmbbzFSflSRzPBBlBbS", "JCjnjTZTTGPSGmTFPb", "ZWHhJjHLDVDgHLLDGjnhctsstwqctNwWqNwwQrtv", "sDwQhcwhBDDwrhGsQnRBQHHMHHMNJMZFCFRbCRftMM", "zjjlmjqfdTqlWdzTqmLzlzVjNCHJNHNFMFtbJNZgVNMMCCtN", "vPTfLmPTLWBsPDnSscnS", "ngznwDPPTzhPPDCTQnTTDQBQqHNNrHFVppbbjRFFqFhHqRqr", "tZJtcGsGtLLcctRqVBbbqrspbHNq", "BmZSvGBMdWPzMPgnnz", "MpNWPVNWWZWVVNZHVcvJjgBjJMStMJSjjg", "rzdCzrCTTLRCslvJDSjjdScgDm", "RLhCQzqTCssThRQzRzwGQrrCFffbfWppNpWNWVcHqZbHpVPp", "zQzCVWdSSjCdjpchWcGftflGZcgG", "RwnJTJwmvFHTBFmtBccZZfBGMstllM", "wwvvHRwqDnHFrmqnrSbQVVQfSbqQjbqjbQ", "ttDftStSlftPgSHmJbFwnMnFwzbrLHMMzz", "GqTBqhBqBvppBvMMTznrCbCnLwfr", "RBjjpZZvvZqGcNhjjpNmDPfDcsgfDfgScsQQQg", "rsSFccvBHppHPsvQrSHSprFjnbLGdbzLfbGLLtLjjzLzvl", "JWWJhmwwTDTGtnzlhdbtLG", "CWNqWRNCwnCJVppQFFFFNrgHBB", "MSRVnMjnVRVnPlcsrtMtschgDl", "NWHBwJBwBBQCHHqwWQGBNgdrFFtsthcqdltdDsqttq", "CCTTGCNCCBfNJNNWbGGnvVzDSRfDRSZvLPSzRn", "MpRfjRjWpZzzzRzZSpjzZjTCQcGdHLWNGqdBdcBWWBLccn", "lrbrsPQDPQglDtwggcLCqnCdNNdHBLsqNd", "blwbJggvgbwlvQbvtgwmvVwRfTzfMMjFVfSFjZjMTSTSzj", "ttSGjHWVrwWrWWvhzvhmhDfR", "qMBdNNsccQgfDRzRmqlhRl", "gQJdfJPdQBsMggMjPrTCLjrGrCrtVT", "tGFdlwDwGFdNtStghWWdQFSnTVfCfZhrfVTVCVprnRRhVn", "cLsBPQJsQPmbmPHTnRRnHprCVfns", "MjmvPqqQjPbQzjLwwDWDSlzSlGSgwl", "NSCpFgfbscbZZZwrtgPZJT", "zGCQlVGmmQGVqqJwGtHZGrPHHRTH", "qQvVmvzmqCdhhjzCQLjljLQMnMDSFWcSfnMfpbfnNcFFbFDN", "zFgqjQBmWNlWlfHrHdLc", "wnbCpSSZZTJSJSnmdrtHfGtftlcpltpH", "ZSwhVPPJgNVmNFzs", "WNVJthVHRRfLqpqN", "gdCGcCgJBCrgScRLzbjQQLfRRR", "SCFdGSFvlhTJsnvW", "FFZwFZZwRmFFhHtNLNLGRtsqjLMt", "gbDnnrMbMCffMPbPLNjGNlcppNtspp", "rgbzrzDrgVgnrBzFWMWmWBwHWHShSB", "zjRVjDqzRjvSBnBGGsfsrFsV", "fLccLLZpJMctwJWWWJWpJGCwFwsgnngFBPsCnnTBPT", "LbJlZNWMtpMlHRNHzdfSDfdj", "VGbbnJGSTsVTssTTnVVWMtfBBmvftRHfHBMJJfZp", "ghqtrzgPrjdzQCjmZMHfRHZHBmQmmB", "gltFtDqFVlTVWlTl", "HqNqZDTvNvVTLPSTvzfrfHfdndffwnbdnwrH", "MpSJlFcMJmcpFlmClcMcRnWbWtthrnfwnCGrrWfrwC", "RjcJJmSFMRQpMRFjMNVvTZjNPPvLTBPBBB", "MzClDtlzJzFzNGGm", "bjcLRHlTBsFJGmRm", "HcPSSfTSpLZLbSwtrtvMnlDCDPCl", "gWWgQJCsVhgRLCWsdjpmcBHvfvrrnvCvBB", "TqDtztqtStlbNTPtllqZpvmcFHjNjvjNvHvmrrmj", "ztPPGZqTPSbJgchGgwRQgQ", "wVrdtTqtCCvbNgbNTTDN", "mhGzWhGzMGWGrRmbFLBHZRNHNvZvgB", "hhGhShpnsSrqVCVSSj", "HnlbmGnlHZHnlBcjgwfDVfwLsGLGLDgR", "WhWMWTvQPWPLDMFRCDMsVD", "QdzJQPSPZqJnJRnZ", "TTjTjFBcRBGjwsDTBLmrCftfRVrrCftCVNRP", "WnqbJWnnQJhSqVfVPfDnggfrVN", "hSlDMllvhbQqllZlSWQdSQTBjsFHBjTwGdHBTBszLzcc", "rNWqWDLZWcqFqLLLgQQJnndnQdNzzJVMzd", "cPtsPvChtRsGswHPGbwcPcdVnpzvnmBmVvJBJdJVJdzn", "PfRfRGtsHsSRftbbbbHhwCCsjZgSTgSZWDTcgDZjLqgqFWLq", "BNzPnPJNNMwHJRhBGRWRdjFQddFlFjWd", "npbZrgnLSCSrWFjjdlZGlcDF", "gqtmmngSbmgHJqfqzNBHBJ", "stgzttBPRRRdpSVVpdpS", "WJFcLQmJZHcCFLJmcZLMfbpGSWMNpGftSSpMrb", "mcvvQvHmtLCJmHZQHZHCDHJJnjqPjjPzwvwhnwPqTjPBzPnB", "DDmbbPqgFSbSQPtPQJttrltJ", "CRfcnZWmRRhJNVtsVnQlsp", "fcBvWvWzcZWCzTTCTTvccFMBHGDdSwGFFbqwFSGSmF", "tCRBPCPRjzsJszBmtjmCvSpHcppJpvZdHHHcHZJG", "qbrlLnWlQDQDNvmHHHHcrZZvdm", "nmnWnnWmgQsCFzFCRVCg", "sHMHCDZfcwMcRcLMcZDCRCHMPdJqgjvVdvqgdgfdJbQgvWQb", "FFhTzmzGrnmtjTBjBBprrmFtqGgQqvVdPbbgqQQWJvvWJQqV", "TrNjrnFSlwNZNlNL", "JQGdsdzSzsdFQFSdssnndNlZjNPTJZNljVjTPhVPhT", "GgGLfRmHGLhNVjjNTLhV", "HvGvwpbHHRwpBrvBgSSzSFndtzndCrsFMd", "DptFshMrhDhDwmPPhwSNhmmS", "RLdcdRvBjnvRVcvlcLbCcbCwwpGBwSmfZqqPPPPwzmfqZq", "VlRLvVjCJLnlpvvRdllLcJJWDHgMMHDDHtWFFDDQgH", "SsSdrndpDlCdLftd", "VGPVJgmQrVGHHZfwLlfCTmfwlDlT", "PcJHcGgcWWbJpjRrphRbFpRn", "PdPSMHMLzPPSShBdffMMzMRHQQrpppcqTCQQpCccTGTRCnCQ", "vsbWmFbmJmZFFsmsbfpGVrGnWrrpVpnnVcTV", "JZslstfZNNSSPdlSMwlM", "bLLzRzZLbRqJJrDGGVZdwssDvGQw", "FCtNJlTFtmPfldSvDvQFVVsjSv", "PmCPHBhhPpWLWgzgHJ", "qwmwFHCgPgPPqPwMCrHHFBVVRBttVRRffVfmsjVNNB", "WSvcSnvbSWbhcbjlQbvlSQhlfBGcspVVsVGfVscpzpNcpBtN", "hJSLhlvlTjPFHMLCCq", "bggDpTggncGVVWbQcG", "sRvSwwwFBSpFzvRvMFZqmPmMVqmcmPPVqhqqWq", "SRBBrFZZwrddBFRjlptnLgDnTggdtd", "PPfMcZMflbMQcMllPVfTVMwjWWmZvpWWpWhhjjpdWWww", "sDQNnzsnQgDNsFzFqtGjGmWWSpWrGhdpvphdSW", "nNQNqLBHLqzDnHgnVJfPJPCRBbfCcJlT", "fppppWsjcSDPjjDpGhgwbfTgHTCbHJwbHbTR", "rLBdQnvMNMmFPbLqHqTqgLHCgL", "nrFznvMNMQdttrBcScsDstWcPGtWSc", "lclnRSDnGZtvSwnZDZzhLffqdsCNwTBCBBdNsd", "mPjmjmrFFpmQjMJQjlNdlhCsCLsTlNLs", "ggmrHHVVQVPJpWrgpWScRvzZzGGRnZvlgzZn", "GjGJGQJGcMTVfFDQzNVQzP", "mHqdbmmdnJqVzVhRVNzPbR", "wStmHJsJsLZLjTvM", "QssMbVGdMQjZPjwVwHVZPZClllvgSgvlTgwwSSCgSCtC", "WmmFBmJrcFRBFrJJBFchzWCStgCTgvhvTlfsNqfTlvTv", "sFzzssDLzrBPjDVVddHMQD", "fztDZSGrNrlnbnPTgFFpln", "jvvQMMcLcjJmQwHdJvjQJnnbTbRFRphnnpsWgmFRPR", "vTLHHCQLHBBjJCSZrVCZtSfSfrft", "gHfHffHLjwHrRjLrLRZVMnTdTBsNTBwTVBsBnN", "DWPhqhhDhvSGvWPzSzMBQBQVMMBBmvssvQvQ", "CDGbqCDbChSbWGrHcHRgbcVcfrLJ", "frlTLmtllbbbdpJS", "qFjhzjThjHTFGHTjqhhjMzBhVpVpdbBnSJQRpBnVVdbRRQJd", "vjWPWjWPPPWgwmfCrNvTvZ"]
+
+# ----- PART 1 -----
+def find_common_item1(compartment1, compartment2):
+    # Loop through each item in the first compartment
+    for item in compartment1:
+      # Check if the item is also in the second compartment
+      if item in compartment2: # Case sensitive
+        print("found matching item:", item)
+        # Calculate & return the priority of the item
+        if item.islower():
+          return ord(item) - ord('a') + 1
+        else:
+          return ord(item) - ord('A') + 27
+        # Break out of the loop since we found the first common item
+        
+
+def count_total_sum1(rucksacks):
+  total_sum = 0
+
+  # Loop through each rucksack
+  for rucksack in rucksacks:
+    # Split the rucksack into its two compartments
+    first_compartment = rucksack[:len(rucksack)//2]
+    second_compartment = rucksack[len(rucksack)//2:]
+    
+    # Add the priority to the total sum
+    total_sum += find_common_item1(first_compartment, second_compartment)
+
+  return total_sum
+
+# Test the function with the example rucksacks
+rucksacks = ['vJrwpWtwJgWrhcsFMMfFFhFp', 'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL', 'PmmdzqPrVvPwwTWBwg', 'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn', 'ttgJtRGJQctTZtZT', 'CrZsJsPPZsGzwwsLwLmpwMDw']
+# print("Example:", count_total_sum1(rucksacks))  # Should print 157
+
+# Part 1 solution
+# print("Part 1 solution:", count_total_sum1(inputRucksacks))
+
+
+# ----- PART 2 -----
+group_size = 3
+
+def find_common_item2(compartments):
+    priority = 0
+    
+    # Loop through each item in the first compartment
+    for item in compartments[0]:
+      # Check if the item is also in other compartments
+      if all(item in compartment for compartment in compartments): # Case sensitive
+        print("found matching item:", item)
+        # Calculate & return the priority of the item
+        if item.islower():
+          priority = ord(item) - ord('a') + 1
+        else:
+          priority = ord(item) - ord('A') + 27
+        break
+        # Break out of the loop since we found the first common item
+
+    print("item priority:", priority)
+    return priority
+  
+        
+def find_common_item_per_group(rucksacks):
+    total_sum = 0
+    group = []
+
+    for index, rucksack in enumerate(rucksacks):
+        group.append(rucksack)
+        
+        if len(group) % group_size == 0:
+            print("--- Group ---")
+            total_sum += find_common_item2(group)
+            group.clear()
+
+    return total_sum
+
+# Part 2 solution
+print("\nPart 2 solution:", find_common_item_per_group(inputRucksacks))
